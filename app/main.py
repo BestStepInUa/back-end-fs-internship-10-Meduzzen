9e07bc0ef7ad9d9505a133d00a32d6bdc76989b0
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2025-02-25 17:38:06 +0200

    imports bug

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from fastapi.staticfiles import StaticFiles
 
-from app.routers import base_router
+from app.routers.__init__ import base_router
 
 
 # asynccontextmanager
@@ -29,3 +30,6 @@ app.include_router(base_router)
 
 # Обслуговування статичних файлів
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(app,  reload=True, host="0.0.0.0", port=8000)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from back-end-fs-internship-10-meduzzen!")
-
-
-if __name__ == "__main__":
-    main()
