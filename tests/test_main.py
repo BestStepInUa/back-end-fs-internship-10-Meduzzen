f9e2ee830a515d7287fd42d92afbe948cdbbbfd6
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2025-03-03 18:34:33 +0200

    still bug but 2 tests work

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
+from app.db.redis import get_redis
 from app.main import app
+import redis.asyncio as redis
+from redis.exceptions import ConnectionError
 
 # Successful case test
 def test_read_root_success():
@@ -14,3 +17,25 @@ def test_read_root_success():
         "detail": "ok",
         "result": "working"
         }
+    
+# Фікстура для асинхронного Redis клієнта
+@pytest.fixture(scope="module")
+async def redis_client():
+    # Створюємо підключення
+    client = redis.Redis.from_url("redis://localhost:6379/0")
+    yield client  # Повертаємо об'єкт клієнта
+    await client.close()  # Закриваємо з'єднання
+
+# Асинхронний тест для Redis
+@pytest.mark.asyncio
+async def test_redis_connection(redis_client):
+    try:
+        # Перевірка підключення
+        assert await redis_client.ping() is True
+        
+        # Тест запису/читання даних
+        await redis_client.set("test_key", "value")
+        value = await redis_client.get("test_key")
+        assert value == b"value"
+    except ConnectionError:
+        pytest.fail("Помилка підключення до Redis")    
